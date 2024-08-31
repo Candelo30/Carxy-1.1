cec68f2d7f84e198feacda63c46eda88d64afb41
Candelo30 <luiscandelo30@gmail.com>
2024-08-30 20:59:34 -0500

    Resolución de conflictos y combinación de ramas

--- AppCarxy/src/app/app.config.ts
+++ AppCarxy/src/app/app.config.ts
@@ -1,32 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-<<<<<<< HEAD
+
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
   withFetch,
   withInterceptors,
 } from '@angular/common/http';
+
 import { CookieService } from 'ngx-cookie-service';
 import { authInterceptor } from './interceptor/auth.interceptor';
-=======
-import { provideHttpClient, withFetch } from '@angular/common/http';
->>>>>>> 057c3682d38b9e5e2f3edcf83fdea8cb6cf0d7d0
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-<<<<<<< HEAD
-    provideHttpClient(withInterceptors([authInterceptor])),
-
-    CookieService, // Asegúrate de incluir el servicio de cookies
-=======
-    provideHttpClient(withFetch()),
->>>>>>> 057c3682d38b9e5e2f3edcf83fdea8cb6cf0d7d0
+    // Configura el HttpClient con el interceptor y fetch
+    provideHttpClient(
+      withFetch(), // Usar Fetch API en lugar de XMLHttpRequest
+      withInterceptors([authInterceptor]) // Registra el interceptor de autenticación
+    ),
+    // Asegúrate de incluir el servicio de cookies
+    CookieService,
   ],
 };
--- AppCarxy/src/app/interceptor/auth.interceptor.ts
+++ AppCarxy/src/app/interceptor/auth.interceptor.ts
@@ -1,21 +1,18 @@
-import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { UsuariosService } from '../service/users/usuarios.service';
+import { inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const usuariosService = inject(UsuariosService);
-  const token = usuariosService.getToken();
+  const cookieService = inject(CookieService);
+  const token = cookieService.get('Token');
 
-  console.log('Token:', token); // Verifica que el token no sea null o undefined
+  const authReq = token
+    ? req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    : req;
 
-  if (token) {
-    const authreq = req.clone({
-      setHeaders: {
-        Authorization: `Token ${token}`, // Asegúrate de usar el prefijo correcto
-      },
-    });
-    return next(authreq);
-  }
-
-  return next(req);
+  return next(authReq);
 };
